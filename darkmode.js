81473b3cf8721bac214aa38a00361675ccd49ac5
tartiilonline <shibane25@gmail.com>
2025-07-07 17:24:59 +0300

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -6,5 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
   toggle?.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     localStorage.setItem('darkMode', document.body.classList.contains('dark') ? 'enabled' : 'disabled');
+    <script src="script.js"></script>
   });
 });
